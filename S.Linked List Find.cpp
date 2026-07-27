b047f1ee636742dd04638e6fa739293f5bb97c17
Assmaa Baj <elsabaaj@gmail.com>
2026-07-27 19:27:53 +0300

    Create S.Linked List Find.cpp

---
+++ S.Linked List Find.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+// Create a node
+class Node
+{
+public:
+    int value;
+    Node* next;
+};
+
+void InsertAtBeginning(Node*& head, int value)
+{
+    // Allocate memory to a node
+    Node* new_node = new Node();
+
+    // insert the data
+    new_node->value = value;
+    new_node->next = head;
+
+    // Move head to new node
+    head = new_node;
+
+}
+
+Node *Find(Node* head, int Value)
+{
+
+    while (head != NULL) {
+
+        if (head->value == Value)
+            return head;
+      // as if we say return true
+
+
+        head = head->next;
+      //this line will make the head moving 
+    }
+
+    return NULL;
+  as if we say return false;
+
+
+}
+
+
+// Print the linked list
+void PrintList(Node* head)
+
+{
+    while (head != NULL) {
+        cout << head->value << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    InsertAtBeginning(head, 1);
+    InsertAtBeginning(head, 2);
+    InsertAtBeginning(head, 3);
+    InsertAtBeginning(head, 4);
+    InsertAtBeginning(head, 5);
+
+    PrintList(head);
+
+    Node* N1 = Find(head, 2);
+
+    if (N1 != NULL)
+        cout << "\nNode Found :-)\n";
+    else
+        cout << "\nNode Is not found :-(\n";
+
+
+    system("pause>0");
+
+}
